af8acbe880ee7a24baf4350133a56ebb0c2c57d1
AndrusovN <andrusov.n@gmail.com>
2020-12-06 21:57:07 +0300

    misclick fix

--- app/src/main/java/kennel/chemscheme/positionProcessing/Structure3D.kt
+++ app/src/main/java/kennel/chemscheme/positionProcessing/Structure3D.kt
@@ -2,7 +2,6 @@ package kennel.chemscheme.positionProcessing
 
 import android.util.Log
 
-<<<<<<< HEAD
 //То же, что и Structure, но вместо Atom использует Atom3D
 class Structure3D(var vertices: MutableList<Atom3D> = mutableListOf()) {
 
