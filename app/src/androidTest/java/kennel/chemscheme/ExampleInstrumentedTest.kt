0a740f753745a6614e0477232473b9c31c699cbb
Alexey <alexey.nebolsin.jr@gmail.com>
2020-12-20 19:09:37 +0300

    added getByType function to MolStruct

--- app/src/androidTest/java/kennel/chemscheme/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/kennel/chemscheme/ExampleInstrumentedTest.kt
@@ -2,11 +2,13 @@ package kennel.chemscheme
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kennel.chemscheme.structure.MolStruct
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +16,9 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class TestAvailableSights {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("kennel.chemscheme", appContext.packageName)
+    fun test1() {
+        //val struct =
     }
 }
